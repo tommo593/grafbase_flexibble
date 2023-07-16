1f5beca6342c5aad17416ddf70466c2383cd1b4b
Tommo <tomdevwork@gmail.com>
2023-07-17 00:31:41 +0100

    All files complete

--- common.types.ts
+++ common.types.ts
@@ -61,3 +61,39 @@ export interface ProjectForm {
   githubUrl: string;
   category: string;
 }
+export interface UserProfile {
+    id: string;
+    name: string;
+    email: string;
+    description: string | null;
+    avatarUrl: string;
+    githubUrl: string | null;
+    linkedinUrl: string | null;
+    projects: {
+      edges: { node: ProjectInterface }[];
+      pageInfo: {
+        hasPreviousPage: boolean;
+        hasNextPage: boolean;
+        startCursor: string;
+        endCursor: string;
+      };
+    };
+}
+
+export interface SessionInterface extends Session {
+  user: User & {
+    id: string;
+    name: string;
+    email: string;
+    avatarUrl: string;
+  };
+}
+
+export interface ProjectForm {
+  title: string;
+  description: string;
+  image: string;
+  liveSiteUrl: string;
+  githubUrl: string;
+  category: string;
+}
---
+++ components/Button.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { MouseEventHandler } from "react";
+
+type Props = {
+    title: string;
+    leftIcon?: string | null;
+    rightIcon?: string | null;
+    handleClick?: MouseEventHandler;
+    isSubmitting?: boolean;
+    type?: 'button' | 'submit';
+    bgColor?: string;
+    textColor?: string;
+}
+
+const Button = ({ title, leftIcon, rightIcon, handleClick, isSubmitting, type, bgColor, textColor }: Props) => {
+  return (
+<button
+type={type || 'button'}
+disabled={isSubmitting}
+className="flexCenter gap-3 px-4 py-3"
+onClick={handleClick}
+>
+{leftIcon && <Image src={leftIcon} width={14} height={14} alt="left" />}
+{title}
+{rightIcon && <Image src={rightIcon} width={14} height={14} alt="right" />}
+
+</button>
+  )
+}
+
+export default Button
