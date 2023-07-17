2b0c5ec0b8e61a4139cd332618d9cd08c1677984
Tommo <tomdevwork@gmail.com>
2023-07-17 21:27:47 +0100

    Error fixed. Now loads

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@ import { ProjectForm } from '@/common.types';
 import { createProjectMutation, createUserMutation, deleteProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery, updateProjectMutation } from '@/graphql';
 import { GraphQLClient } from 'graphql-request';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV2 === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
